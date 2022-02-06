faa8f18761e88b6df26bf0645430729fc1ddff88
prajwalpraveen97 <prajwalpraveen97@gmail.com>
2022-02-06 14:09:00 +0100

    Code Optimization

--- MyProjectWork/ExperimentOnMultiImageLearning/MultiImageLearning/Program.cs
+++ MyProjectWork/ExperimentOnMultiImageLearning/MultiImageLearning/Program.cs
@@ -19,15 +19,15 @@ namespace MultiImageLearningExperiment
         {
             //
             // Starts experiment that demonstrates how to learn spatial patterns.
-            SpatialPatternLearning experiment = new SpatialPatternLearning();
-            experiment.Run();
+            //SpatialPatternLearning experiment = new SpatialPatternLearning();
+            //experiment.Run();
 
             //
             // Starts experiment that demonstrates how to learn spatial patterns.
             //SequenceLearning experiment = new SequenceLearning();
             //experiment.Run();
 
-            //RunMultiSimpleSequenceLearningExperiment();
+            RunMultiSimpleSequenceLearningExperiment();
             //RunMultiImageLearningExperiment();
         }
 
@@ -38,7 +38,7 @@ namespace MultiImageLearningExperiment
             sequences.Add("S1", new List<double>(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, }));
             sequences.Add("S2", new List<double>(new double[] { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 }));
 
-            //
+            
             // Prototype for building the prediction engine.
             MultiImageLearning experiment = new MultiImageLearning();
             var predictor = experiment.Run(sequences);
