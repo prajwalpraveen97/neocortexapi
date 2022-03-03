b543911ab67c45dde610903a6de9a2df49ecc597
prajwalpraveen97 <prajwalpraveen97@gmail.com>
2022-03-03 20:30:20 +0100

    Code Optimization

--- MyProjectWork/SequenceLearningExperiments/SequenceLearningExperiments/HelperMethods.cs
+++ MyProjectWork/SequenceLearningExperiments/SequenceLearningExperiments/HelperMethods.cs
@@ -73,97 +73,8 @@ namespace SequenceLearningExperiment
             }
             return null;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="dataFilePath"></param>
-        /// <returns></returns>
-        /// 
 
         /*
-        public static Dictionary<float[][], float[][]> ReadCancerSequencesDataFromFile_LSTM_v2(string dataFilePath)
-        {
-            List<Dictionary<string, string>> SequencesCollection = new List<Dictionary<string, string>>();
-
-            int keyForUniqueIndexes = 0;
-
-            if (File.Exists(dataFilePath))
-            {
-
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        var line = sr.ReadLine();
-                        string[] values = line.Split(",");
-
-                        Dictionary<string, string> Sequence = new Dictionary<string, string>();
-                        int count = 0;
-
-                        string label = values[1];
-                        string sequenceString = values[0];
-
-                        if (sequenceString.Length < 33)
-                        {
-                            int remainingLength = 33 - sequenceString.Length;
-                            for (int i = 0; i < remainingLength; i++)
-                            {
-                                sequenceString = sequenceString + "Z";
-                            }
-                        }
-                        Sequence.Add(sequenceString, label);
-                        SequencesCollection.Add(Sequence);
-                    }
-                }
-
-                float[][] processedDataSet = new float[SequencesCollection.Count][];
-                float[][] processedLabel = new float[SequencesCollection.Count][];
-
-                int index = 0;
-                foreach (var sequence in SequencesCollection)
-                {
-
-                    var sequenceDict = sequence;
-                    var sequenceString = sequenceDict.Keys.ElementAt(0);
-                    var sequenceLabel = sequenceDict.Values.ElementAt(0).Split("_")[0];
-
-                    float[] sequenceProcessed = new float[0];
-
-                    foreach (var element in sequenceString)
-                    {
-                        var numericval = char.ToUpper(element) - 64;
-                        sequenceProcessed = sequenceProcessed.Concat(new float[] { numericval }).ToArray();
-                    }
-
-                    processedDataSet[index] = sequenceProcessed;
-                    var label_encoded = new float[0];
-                    if (sequenceLabel == CancerSequenceClasses[0])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[0];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[1])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[1];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[2])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[2];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[3])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[3];
-                    }
-                    processedLabel[index] = label_encoded;
-                    index++;
-                }
-                var returnDict = new Dictionary<float[][], float[][]>();
-                returnDict.Add(processedDataSet, processedLabel);
-
-                return returnDict;
-            }
-            return null;
-        }
-        */
         /// <summary>
         /// 
         /// </summary>
@@ -214,6 +125,7 @@ namespace SequenceLearningExperiment
             }
             return null;
         }
+        */
     
         //*********************************************************************************************
         // ENCODING TRAINING DATA
@@ -264,7 +176,7 @@ namespace SequenceLearningExperiment
             return ListOfEncodedTrainingSDR;
         }
 
-
+        /*
         public static Dictionary<string, int[]> EncodeCancerSequencesApproach2(List<Dictionary<string, string>> trainingData)
         {
 
@@ -298,6 +210,7 @@ namespace SequenceLearningExperiment
             }
             return tempDictionary;
         }
+        */
 
         public static Dictionary<float[][], float[][]> CancerSequenceDataProcessing(List<Dictionary<string, string>> trainingData)
         {
